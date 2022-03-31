eeab850a1cf5cf362ac46a4521d64653ea7ca601
LVNA <aislinge858@ajou.ac.kr>
2022-03-31 17:15:16 +0900

    commit 2

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-const url = "";
+const url =
+  "https://raw.githubusercontent.com/LVNAY/capston1week5/main/ajou.json";
 
 async function getData() {
   const response = await axios.get(url);
-  console.log(response);
+  return response.data;
 }
 
-getData();
+async function makeItem() {
+  //1. 데이터 가져오기
+  const data = await getData();
+
+  // 2. 이름 넣기
+
+  const nameElement = document.querySelector("#name");
+  const korean = data["이름"]["한글"];
+  const english = data["이름"]["영문"];
+  const chinese = data["이름"]["한문"];
+  nameElement.innerHTML = korean + "<br>" + chinese + "<br>" + english;
+}
